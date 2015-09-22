8d1807e3400f823503913af456e30d08d0cbf1ce
Josh Cain <jcain@redhat.com>
2015-09-21 20:55:06 -0400

    Add licensing to source files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jaxbRss
+
+[![Build Status](https://travis-ci.org/cainj13/jaxbRss.svg?branch=master)](https://travis-ci.org/cainj13/jaxbRss)
+
 JAXB Binding for RSS objects
--- src/test/java/org/semper/reformanda/syndication/rss/CategoryTest.java
+++ src/test/java/org/semper/reformanda/syndication/rss/CategoryTest.java
@@ -1,8 +1,21 @@
+/**
+ * Copyright 2015 Joshua Cain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.semper.reformanda.syndication.rss;
 
-import org.custommonkey.xmlunit.NamespaceContext;
-import org.custommonkey.xmlunit.SimpleNamespaceContext;
-import org.custommonkey.xmlunit.XMLUnit;
 import org.custommonkey.xmlunit.XpathEngine;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,13 +23,6 @@ import org.semper.reformanda.syndication.rss.itunes.Category;
 import org.semper.reformanda.syndication.rss.itunes.ItunesCategory;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
